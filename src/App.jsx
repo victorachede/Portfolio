dda392cfd5ad491803692095b151ab3875f5c94a
Victor <victorachede@gmail.com>
2026-02-16 07:43:39 -0800

    chore: sync local changes before rebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from 'react-hot-toast'; // Import Toaster
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import Blog from "./pages/Blog";
+// import Blog from "./pages/Blog";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Courses from "./pages/Courses";
@@ -15,7 +15,7 @@ const App = () => {
       <main className="pt-20 px-4 md:px-8">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
+          {/* <Route path="/blog" element={<Blog />} /> */}
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/courses" element={<Courses />} />
